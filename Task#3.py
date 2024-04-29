5128d11d9303af166dddd82aa43a8957c8711f4e
Marat Abyshev <166024789+hanz0m4@users.noreply.github.com>
2024-04-29 23:22:29 +0300

    Update Task#3.py

--- Task#3.py
+++ Task#3.py
@@ -1,8 +1,7 @@
 import math
 
 
-class figure:
-
+class Figure:
 
     def area(self):
         pass
@@ -12,21 +11,20 @@ class figure:
         pass
 
 
-    def sravnenie_per(self, other):
+    def compare_per(self, other):
         if self.perimetr() > other.perimetr():
-            return "больше"
+            return True
         else:
-            return "меньше"
+            return False
 
 
-    def sravnenie_area(self, other):
+    def compare_area(self, other):
         if self.area() > other.area():
-            return "больше"
+            return True
         else:
-            return "меньше"
+            return False
 
-
-class krug(figure):
+class Circle(Figure):
 
     def __init__(self, radius):
         self.radius = radius
@@ -37,109 +35,109 @@ class krug(figure):
         return math.pi * self.radius ** 2
 
 
-class kvadrat(figure):
+class Square(Figure):
 
-    def __init__(self, storona):
-        self.storona = storona
-
-    def perimetr(self):
-        return self.storona * 4
-
-    def area(self):
-        return self.storona ** 2
-
-
-class pryamougolnik(figure):
-
-    def __init__(self, dlina, shirina):
-        self.dlina = dlina
-        self.shirina = shirina
+    def __init__(self, side):
+        self.side = side
 
     def perimetr(self):
-        return (self.dlina + self.shirina)*2
+        return self.side * 4
 
     def area(self):
-        return self.dlina * self.shirina
+        return self.side ** 2
 
 
-class treugolnik(figure):
+class Rectangle(Figure):
 
-    def __init__(self, gipotenuza, katet1, katet2):
-        self.gipotenuza = gipotenuza
-        self.katet1 = katet1
-        self.katet2 = katet2
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
 
     def perimetr(self):
-        return self.gipotenuza + self.katet1 + self.katet2
+        return (self.length + self.width)*2
 
     def area(self):
-        return 0.5 * (self.katet1 + self.katet2)
+        return self.length * self.width
+
+
+class Triangle(Figure):
+
+    def __init__(self, hypotenuse, kathete1, kathete2):
+        self.hypotenuse = hypotenuse
+        self.kathete1 = kathete1
+        self.kathete2 = kathete2
+
+    def perimetr(self):
+        return self.hypotenuse + self.kathete1 + self.kathete2
+
+    def area(self):
+        return 0.5 * (self.kathete1 + self.kathete2)
 
 
 
-k1 = kvadrat(6)
+k1 = Square(6)
 print("Площадь квадрата 1:", k1.area())
 print("Периметр квадрата 1:", k1.perimetr())
 
 
-p1 = pryamougolnik(4, 6)
+p1 = Rectangle(4, 6)
 print("Площадь прямоугольника 1:", p1.area())
 print("Периметр прямоугольника 1:", p1.perimetr())
 
 
-t1 = treugolnik(5, 4, 3)
+t1 = Triangle(5, 4, 3)
 print("Площадь треугольника 1:", t1.area())
 print("Периметр треугольника 1:", t1.perimetr())
 
 
-k1 = krug(5)
+k1 = Circle(5)
 print("Площадь круга 1:", k1.area())
 print("Периметр круга 1:", k1.perimetr())
 print (" ")
 
 
-print("Сравнение периметра квардрата с прямоугольником:", k1.sravnenie_per(p1))
-print("Сравнение периметра квардрата с треугольником:", k1.sravnenie_per(t1))
-print("Сравнение периметра квардрата с кругом:", k1.sravnenie_per(k1))
+print("Сравнение периметра квардрата с прямоугольником:", k1.compare_per(p1))
+print("Сравнение периметра квардрата с треугольником:", k1.compare_per(t1))
+print("Сравнение периметра квардрата с кругом:", k1.compare_per(k1))
 print (" ")
 
 
-print("Сравнение периметра прямоугольника с квадратом:", p1.sravnenie_per(k1))
-print("Сравнение периметра прямоугольника с треугольником:", p1.sravnenie_per(t1))
-print("Сравнение периметра прямоугольника с кругом:", p1.sravnenie_per(k1))
+print("Сравнение периметра прямоугольника с квадратом:", p1.compare_per(k1))
+print("Сравнение периметра прямоугольника с треугольником:", p1.compare_per(t1))
+print("Сравнение периметра прямоугольника с кругом:", p1.compare_per(k1))
 print (" ")
 
 
-print("Сравнение периметра круга с прямоугольником:", k1.sravnenie_per(p1))
-print("Сравнение периметра круга с треугольником:", k1.sravnenie_per(t1))
-print("Сравнение периметра круга с квадратом:", k1.sravnenie_per(k1))
+print("Сравнение периметра круга с прямоугольником:", k1.compare_per(p1))
+print("Сравнение периметра круга с треугольником:", k1.compare_per(t1))
+print("Сравнение периметра круга с квадратом:", k1.compare_per(k1))
 print (" ")
 
 
-print("Сравнение периметра треугольника с прямоугольником:", t1.sravnenie_per(p1))
-print("Сравнение периметра треугольника с квадрата:", t1.sravnenie_per(k1))
-print("Сравнение периметра треугольника с кругом:", t1.sravnenie_per(k1))
+print("Сравнение периметра треугольника с прямоугольником:", t1.compare_per(p1))
+print("Сравнение периметра треугольника с квадрата:", t1.compare_per(k1))
+print("Сравнение периметра треугольника с кругом:", t1.compare_per(k1))
 print (" ")
 
 
-print("Сравнение площади квардрата с прямоугольником:", k1.sravnenie_area(p1))
-print("Сравнение площади квардрата с треугольником:", k1.sravnenie_area(t1))
-print("Сравнение площади квардрата с кругом:", k1.sravnenie_area(k1))
+print("Сравнение площади квардрата с прямоугольником:", k1.compare_area(p1))
+print("Сравнение площади квардрата с треугольником:", k1.compare_area(t1))
+print("Сравнение площади квардрата с кругом:", k1.compare_area(k1))
 print (" ")
 
 
-print("Сравнение площади прямоугольника с квадратом:", p1.sravnenie_area(k1))
-print("Сравнение площади прямоугольника с треугольником:", p1.sravnenie_area(t1))
-print("Сравнение площади прямоугольника с кругом:", p1.sravnenie_area(k1))
+print("Сравнение площади прямоугольника с квадратом:", p1.compare_area(k1))
+print("Сравнение площади прямоугольника с треугольником:", p1.compare_area(t1))
+print("Сравнение площади прямоугольника с кругом:", p1.compare_area(k1))
 print (" ")
 
 
-print("Сравнение площади круга с прямоугольником:", k1.sravnenie_area(p1))
-print("Сравнение площади круга с треугольником:", k1.sravnenie_area(t1))
-print("Сравнение площади круга с квадратом:", k1.sravnenie_area(k1))
+print("Сравнение площади круга с прямоугольником:", k1.compare_area(p1))
+print("Сравнение площади круга с треугольником:", k1.compare_area(t1))
+print("Сравнение площади круга с квадратом:", k1.compare_area(k1))
 print (" ")
 
 
-print("Сравнение площади треугольника с прямоугольником:", t1.sravnenie_area(p1))
-print("Сравнение площади треугольника с квадратом:", t1.sravnenie_area(k1))
-print("Сравнение площади треугольника с кругом:", t1.sravnenie_area(k1))
+print("Сравнение площади треугольника с прямоугольником:", t1.compare_area(p1))
+print("Сравнение площади треугольника с квадратом:", t1.compare_area(k1))
+print("Сравнение площади треугольника с кругом:", t1.compare_area(k1))
